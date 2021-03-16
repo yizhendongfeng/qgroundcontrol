dbe3f341b4fbf169f3e906ab9caf4842c40ebdda
zhangjimin <869159813@qq.com>
2021-03-16 17:48:49 +0800

    与综合信息处理系统通信

---
+++ src/comm/IIPSComm.cpp
@@ -0,0 +1,391 @@
+﻿#include <QDebug>
+#include <QSettings>
+#include <QNetworkDatagram>
+#include <QFile>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QTimer>
+#include <QDateTime>
+
+#include "mavlink.h"
+#include "../Vehicle/Vehicle.h"
+#include "QGCApplication.h"
+#include "SettingsManager.h"
+#include "IIPSComm.h"
+
+enum AltitudeMode {
+    AltitudeModeNone,           // Being used as distance value unrelated to ground (for example distance to structure)
+    AltitudeModeRelative,       // MAV_FRAME_GLOBAL_RELATIVE_ALT
+    AltitudeModeAbsolute,       // MAV_FRAME_GLOBAL
+    AltitudeModeAboveTerrain,   // Absolute altitude above terrain calculated from terrain data
+    AltitudeModeTerrainFrame    // MAV_FRAME_GLOBAL_TERRAIN_ALT
+};
+
+IIPSComm::IIPSComm(QObject *parent) : QObject(parent)
+{
+    QSettings settings;
+    settings.beginGroup("IIPS");
+    if(settings.contains("iipsIp"))
+        iipsIp = settings.value("iipsIp", "127.0.0.1").toString();
+    else
+    {
+        iipsIp = "127.0.0.1";
+        settings.setValue("iipsIp", iipsIp);
+    }
+    iipsAddress.setAddress(iipsIp);
+    if(settings.contains("iipsPort"))
+        iipsPort = static_cast<uint16_t>(settings.value("iipsPort", 8000).toUInt());
+    else
+    {
+        iipsPort = 8000;
+        settings.setValue("iipsPort", iipsPort);
+    }
+    if(settings.contains("qgcPort"))
+        qgcPort = static_cast<uint16_t>(settings.value("qgcPort", 8001).toUInt());
+    else
+    {
+        qgcPort = 8001;
+        settings.setValue("qgcPort", qgcPort);
+    }
+    settings.endGroup();
+
+    if(!udpSocket.bind(qgcPort, QAbstractSocket::ShareAddress))
+        qWarning("IIPS udp bind to port %d failed", qgcPort);
+    connect(&udpSocket, &QUdpSocket::readyRead, this, &IIPSComm::SlotReceiveData);
+
+    sendPacket.dataLength = statusPacketId;
+    sendPacket.id = statusPacketLength;
+
+    QTimer* timerTest = new QTimer(this);
+    connect(timerTest, &QTimer::timeout, this, &IIPSComm::SlotTestTimeout);
+//    timerTest->start(1000);
+    memset(&receiveBuffer, 0, sizeof(receiveBuffer));
+    timerIipsConnect.setSingleShot(true);
+    connect(&timerIipsConnect, &QTimer::timeout, this, [&](){
+        iipsConnected = false;
+        emit iipsConnectedChanged();
+        qDebug() << "hide";
+    });
+}
+
+IIPSComm::~IIPSComm()
+{
+    QSettings settings;
+    settings.beginGroup("IIPS");
+    settings.setValue("iipsIp", iipsIp);
+    settings.setValue("iipsPort", iipsPort);
+    settings.setValue("qgcPort", qgcPort);
+    settings.endGroup();
+}
+
+int IIPSComm::SaveWaypointsToJsonFile(PacketId type)
+{
+    QString fileName;
+
+    if (type == LINE)
+        fileName = "/IIPSLine.plan";
+    else if (type == REGION || type == SURVEY)
+        fileName = "/IIPSSurvey.plan";
+    else
+        return -1;
+    QFile jsonPlanFile(qgcApp()->toolbox()->settingsManager()->appSettings()->missionSavePath() + fileName);
+    QJsonObject planJson;
+    planJson["groundStation"] = "QGroundControl";
+    planJson["fileType"] = "Plan";
+    planJson["version"] = 1;
+
+    QJsonObject missionJson;
+    QJsonArray coordinateArray;
+    Waypoint waypointHome = listWaypoints.first();
+    coordinateArray << waypointHome.latitude << waypointHome.longitude << 0;
+    QJsonValue jsonValueHome(coordinateArray);
+    missionJson["plannedHomePosition"] = jsonValueHome;
+    missionJson["firmwareType"] = vehicle ? vehicle->firmwareType() : MAV_AUTOPILOT::MAV_AUTOPILOT_PX4;
+    missionJson["vehicleType"] = vehicle ? vehicle->vehicleType() : MAV_TYPE::MAV_TYPE_QUADROTOR;
+    missionJson["cruiseSpeed"] = vehicle ? vehicle->defaultCruiseSpeed() : 15;
+    missionJson["hoverSpeed"] = vehicle ? vehicle->defaultHoverSpeed() : 5;
+    QJsonArray missionItems;
+    if (type == LINE)
+    {
+        for (int i = 1; i < listWaypoints.size(); i++)
+        {
+            QJsonObject jsonObjectItem;
+            jsonObjectItem["type"] = "SimpleItem";
+            jsonObjectItem["frame"] =  MAV_FRAME::MAV_FRAME_GLOBAL_RELATIVE_ALT;
+            jsonObjectItem["command"] = MAV_CMD::MAV_CMD_NAV_WAYPOINT;
+            jsonObjectItem["autoContinue"] = true;
+            jsonObjectItem["doJumpId"] = i;
+            Waypoint waypointTemp = listWaypoints.at(i);
+            QJsonArray rgParams =  { 0, 0, 0, QJsonValue(), waypointTemp.latitude, waypointTemp.longitude, waypointTemp.altitude};
+            jsonObjectItem["params"] = rgParams;
+            jsonObjectItem["AltitudeMode"] = AltitudeMode::AltitudeModeRelative;
+            jsonObjectItem["Altitude"] =  50.0;
+            jsonObjectItem["AMSLAltAboveTerrain"] = 30.0;
+            missionItems.append(jsonObjectItem);
+        }
+    }
+    else if (type == REGION || type == SURVEY)
+    {
+        if (listWaypoints.count() < 4)
+            return -2;
+        QJsonObject jsonObjectItem1;
+        jsonObjectItem1["autoContinue"] = true;
+        jsonObjectItem1["command"] = 530;
+        jsonObjectItem1["doJumpId"] = 1;
+        jsonObjectItem1["frame"] = MAV_FRAME::MAV_FRAME_MISSION;
+        QJsonArray rgParams =  { 0, 2, QJsonValue(), QJsonValue(), QJsonValue(), QJsonValue(), QJsonValue()};
+        jsonObjectItem1["params"] = rgParams;
+        jsonObjectItem1["type"] = "SimpleItem";
+        missionItems.append(jsonObjectItem1);
+
+        QJsonObject jsonObjectItem2;
+        QJsonObject jsonObjectItem2_1;
+        QJsonObject jsonObjectItem2_1_1;
+        jsonObjectItem2_1_1["AdjustedFootprintFrontal"] = 15.424999999999997;
+        jsonObjectItem2_1_1["AdjustedFootprintSide"] = 20.566666666666663;
+        jsonObjectItem2_1_1["CameraName"] = QStringLiteral("自定义相机");
+        jsonObjectItem2_1_1["DistanceToSurface"] = 50;
+        jsonObjectItem2_1_1["DistanceToSurfaceRelative"] = true;
+        jsonObjectItem2_1_1["FixedOrientation"] = false;
+        jsonObjectItem2_1_1["FocalLength"] = 4.5;
+        jsonObjectItem2_1_1["FrontalOverlap"] = 70;
+        jsonObjectItem2_1_1["ImageDensity"] = 1.7138888888888888;
+        jsonObjectItem2_1_1["ImageHeight"] = 3000;
+        jsonObjectItem2_1_1["ImageWidth"] = 4000;
+        jsonObjectItem2_1_1["Landscape"] = true;
+        jsonObjectItem2_1_1["MinTriggerInterval"] = 0;
+        jsonObjectItem2_1_1["SensorHeight"] = 4.55;
+        jsonObjectItem2_1_1["SensorWidth"] = 6.17;
+        jsonObjectItem2_1_1["SideOverlap"] = 70;
+        jsonObjectItem2_1_1["ValueSetIsDistance"] = true;
+        jsonObjectItem2_1_1["version"] = 1;
+        jsonObjectItem2_1["CameraCalc"] = jsonObjectItem2_1_1;
+
+        jsonObjectItem2_1["CameraShots"] = 152;
+        jsonObjectItem2_1["CameraTriggerInTurnAround"] = true;
+        jsonObjectItem2_1["FollowTerrain"] = false;
+        jsonObjectItem2_1["HoverAndCapture"] = false;
+        jsonObjectItem2_1["Items"] = QJsonArray();
+        jsonObjectItem2_1["Refly90Degrees"] = false;
+        jsonObjectItem2_1["TurnAroundDistance"] = 20;
+        jsonObjectItem2_1["VisualTransectPoints"] = QJsonArray();
+        jsonObjectItem2_1["version"] = 1;
+        jsonObjectItem2["TransectStyleComplexItem"] = jsonObjectItem2_1;
+
+        jsonObjectItem2["angle"] = 40;
+        jsonObjectItem2["complexItemType"] = "survey";
+        jsonObjectItem2["entryLocation"] = 0;
+        jsonObjectItem2["flyAlternateTransects"] = false;
+        QJsonArray jsonObjectItem2_2;
+        for (int i = 1; i < listWaypoints.count(); i++)
+        {
+            jsonObjectItem2_2.append(QJsonArray({listWaypoints.at(i).latitude, listWaypoints.at(i).longitude}));
+        }
+        jsonObjectItem2["polygon"] = jsonObjectItem2_2;
+        jsonObjectItem2["splitConcavePolygons"] = false;
+        jsonObjectItem2["type"] = "ComplexItem";
+        jsonObjectItem2["version"] = 5;
+        missionItems.append(jsonObjectItem2);
+    }
+    missionJson["items"] = missionItems;
+    planJson["mission"] = missionJson;
+
+    QJsonObject fenceJson;
+    fenceJson["version"] = 2;
+    QJsonArray jsonPolygonArray;
+    fenceJson["polygons"] = jsonPolygonArray;
+    QJsonArray jsonCircleArray;
+    fenceJson["circles"] = jsonCircleArray;
+    planJson["geoFence"] = fenceJson;
+
+    QJsonObject rallyJson;
+    rallyJson["version"] = 2;
+    QJsonArray rgPoints;
+    rallyJson["points"] = rgPoints;
+    planJson["rallyPoints"] = rallyJson;
+
+    QJsonDocument jsonDoc(planJson);
+    bool opened = jsonPlanFile.open(QIODevice::WriteOnly);
+    qDebug() << jsonPlanFile.fileName() << "open" << opened;
+    if (!opened)
+        return 1;
+    qint64 bytesWroten = jsonPlanFile.write(jsonDoc.toJson());
+    qDebug() << "bytesWroten:" << bytesWroten;
+    jsonPlanFile.close();
+
+    //发送到planview.qml中调用属性_planMasterController的“打开文件”函数
+    emit sigSendPlanFile(jsonPlanFile.fileName());
+    listWaypoints.clear();  //上传后清除
+    return 0;
+}
+
+void IIPSComm::SlotSendData(const QGCStatusData& data)
+{
+    sysId = data.id;
+    BinaryPacket statusPacket = {new uint8_t[statusPacketLength], statusPacketId, statusPacketLength};
+    memcpy(statusPacket.data, &data, static_cast<size_t>(statusPacket.dataLength));
+    BinaryBuffer binaryBuffer = IIPSProtocol::BinaryPacketEncode(statusPacket);
+    delete []statusPacket.data;
+    /*qDebug() << "writeDatagram:" << */udpSocket.writeDatagram((char*)(binaryBuffer.data), binaryBuffer.length, iipsAddress, iipsPort);
+}
+
+void IIPSComm::SlotReceiveData()
+{
+    while (udpSocket.hasPendingDatagrams())
+    {
+        QNetworkDatagram datagram = udpSocket.receiveDatagram(BinaryBufferLength - receiveBuffer.length);
+//        qDebug() << "SlotReceiveData length:" << datagram.data().length() << datagram.senderAddress() << datagram.senderPort();
+        memcpy(receiveBuffer.data + receiveBuffer.length, datagram.data().data(), datagram.data().length());
+        receiveBuffer.length += datagram.data().size();
+        while(true)
+        {
+            receivePacket = IIPSProtocol::BinaryBufferDecode(&receiveBuffer);
+            if (receivePacket.id == IdError)
+                return;
+            switch (receivePacket.id)
+            {
+            case HEARTBEAT:
+                if (!iipsConnected)
+                {
+                    iipsConnected = true;
+                    emit iipsConnectedChanged();
+                    qDebug() << "show";
+                }
+                timerIipsConnect.start(iipsTimeOut * 1000);
+                break;
+            case STANDBY:
+                qgcApp()->showMessage("IIPS command:Standby!");
+            case LINE:
+                if (GetWayPoints(LINE))
+                    SendAck();
+                break;
+            case SURVEY:
+            case REGION:
+                if (GetWayPoints(REGION))
+                    SendAck();
+                break;
+            case FOLLOW:
+                qgcApp()->showMessage("IIPS command: Follow target not ready!");
+                break;
+            default:
+                break;
+            }
+            delete[] receivePacket.data; // Ensure that you free the packet when you done with it or you will leak memory
+        }
+    }
+}
+
+void IIPSComm::SlotTestTimeout()
+{
+    data.id = 1;
+    data.timestamp = 2;
+    data.status = 3;
+    data.latitude = 4;
+    data.longitude = 5;
+    data.altitude = 6;
+    data.roll = 7;
+    data.pitch = 8;
+    data.yaw = 9;
+    data.velocityNorth = 10;
+    data.velocityEast = 11;
+    data.velocityDown = 12;
+    data.gyroscopeX = 13;
+    data.gyroscopeY = 14;
+    data.gyroscopeZ = 15;
+//    data.accelerometerX = 16;
+//    data.accelerometerY = 17;
+//    data.accelerometerZ = 18;
+//    data.satellitesUsed = 19;
+//    data.fixType = 20;
+//    data.battery = 21;
+    SlotSendData(data);
+}
+
+bool IIPSComm::getIipsConnected() const
+{
+    return iipsConnected;
+}
+
+void IIPSComm::SendAck()
+{
+    BinaryPacket responsePacket = {new uint8_t[11], 129, 11};
+    memcpy(responsePacket.data, &sysId, sizeof(uint16_t));
+    responsePacket.data[10] = RESPONSE;
+    BinaryBuffer binaryBuffer = IIPSProtocol::BinaryPacketEncode(responsePacket);
+    if(udpSocket.isValid())
+        udpSocket.writeDatagram((char*)(binaryBuffer.data), binaryBuffer.length, iipsAddress, iipsPort);
+    delete[] responsePacket.data;
+}
+
+bool IIPSComm::GetWayPoints(PacketId type)
+{
+    switch (receivePacket.data[10])
+    {
+    case START:
+        listWaypoints.clear();
+        break;
+    case WAYPOINT:
+        uint16_t id;
+        Waypoint waypoint;
+        memcpy(&id, receivePacket.data, sizeof(uint16_t));
+
+        if (sysId != id)
+        {
+            qgcApp()->showMessage(QString("Receiving mission to not connected vehicle %1").arg(id));
+            qWarning() << "sysId mismatch!";
+            return false;
+        }
+
+        memcpy(&waypoint.timestamp, receivePacket.data + 2, sizeof(double));
+        memcpy(&waypoint.latitude, receivePacket.data + 11, sizeof(double));
+        memcpy(&waypoint.longitude, receivePacket.data + 19, sizeof(double));
+        memcpy(&waypoint.altitude, receivePacket.data + 27, sizeof(float));
+        waypoint.latitude = qRadiansToDegrees(waypoint.latitude);
+        waypoint.longitude = qRadiansToDegrees(waypoint.longitude);
+        if (listWaypoints.count() > 0 && listWaypoints.last().timestamp > waypoint.timestamp)
+        {
+            qWarning() << "new waypoint timestamp is smaller than last one";
+            delete[] receivePacket.data;
+        }
+        listWaypoints.append(waypoint);
+//        qDebug() << "listWaypoints.append(waypoint)" << listWaypoints.count();
+        break;
+    case END:
+        SaveWaypointsToJsonFile(type);
+//        qDebug() << "END SaveWaypointsToJsonFile(type)" << type;
+        break;
+    default:
+        break;
+    }
+    return true;
+}
+
+
+
+Vehicle* IIPSComm::getVehicle() const
+{
+    return vehicle;
+}
+
+void IIPSComm::setVehicle(Vehicle* value)
+{
+    vehicle = value;
+}
+
+void IIPSComm::AppendVehicle(Vehicle* value)
+{
+    if (!vehicles.contains(value))
+        vehicles.append(value);
+}
+
+void IIPSComm::RemoveVehicle(Vehicle* value)
+{
+    for (int i=0; i<vehicles.count(); i++)
+    {
+        if (vehicles[i] == value) {
+            vehicles.removeAt(i);
+            break;
+        }
+    }
+}
---
+++ src/comm/IIPSComm.h
@@ -0,0 +1,154 @@
+﻿/**
+ * @copyright (C) 2021 版权所有
+ * @brief QGroundControl（简写QGC）发送无人机信息至综合信息处理系统（Integrated information processing system，简写IIPS），并接收相关命令及航线文件
+ * 使用UDP进行通信，接收机发送的IP地址及端口可在"C:\Users\当前用户\AppData\Roaming\QGroundControl.org\QGroundControl.ini"中iips组中设置
+ * @author 张纪敏 <869159813@qq.com>
+ * @version 1.0.0
+ * @date 2021-02-17
+ */
+#pragma once
+
+#include <QObject>
+#include <QUdpSocket>
+
+#include "IIPSProtocol.h"
+
+#pragma pack(1)
+struct QGCStatusData        // 发送给综合信息处理系统的无人机状态
+{
+    uint16_t id;            //载具编号
+    double_t timestamp;     //(s)
+    uint8_t status;         //0:故障，1:正常
+    double_t latitude;      //(rad)
+    double_t longitude;     //(rad)
+    double_t altitude;      //(m)
+    float_t roll;           //(rad)
+    float_t pitch;          //(rad)
+    float_t yaw;            //(rad)
+    float_t velocityNorth;  //(m/s)
+    float_t velocityEast;   //(m/s)
+    float_t velocityDown;   //(m/s)
+    float_t gyroscopeX;     //(rad/s)
+    float_t gyroscopeY;     //(rad/s)
+    float_t gyroscopeZ;     //(rad/s)
+//    float_t accelerometerX; //(m/ss)
+//    float_t accelerometerY; //(m/ss)
+//    float_t accelerometerZ; //(m/ss)
+//    uint8_t satellitesUsed; //卫星数
+//    uint8_t fixType;        //定位类型
+//    float_t battery;        //(v)
+};
+
+struct IIPSCommand     //综合信息处理系统发来的命令
+{
+    uint16_t id;                    //载具编号
+    double_t timestamp;
+    uint8_t setting;                //0:锁定，1:解锁，2:起飞，3:悬停，4:跟踪UGV，5:降落，6:盘旋go around，7:返航
+    float_t desiredVelocityNorth;   //期望/目标北向速度(m/s)
+    float_t desiredVelocityEast;    //期望/目标东向速度(m/s)
+    float_t desiredVelocityDown;    //期望/目标地向速度(m/s)
+    float_t desiredHeading;         //期望航向(rad)
+    double_t desiredLatitude;       //(rad)
+    double_t desiredLongitude;      //(rad)
+    double_t desiredAltitude;       //(m)
+};
+typedef struct
+{
+    double timestamp;
+    double latitude;
+    double longitude;
+    float altitude;
+} Waypoint;
+#pragma pack()
+Q_DECLARE_METATYPE(IIPSCommand);
+
+class Vehicle;
+class IIPSComm : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(bool iipsConnected READ getIipsConnected  NOTIFY iipsConnectedChanged)
+    enum UploadType     //data[10]的值
+    {
+        START = 0,      //任务上传开始
+        WAYPOINT = 1,   //航点上传
+        END = 2,        //任务上传结束
+        RESPONSE = 0XFF //QGC应答
+    };
+
+    enum PacketId
+    {
+        HEARTBEAT = 0,    //心跳
+        STANDBY = 1,      //待命
+        SURVEY = 2,       //测绘
+        LINE = 3,         //航线
+        REGION = 4,       //区域
+        FOLLOW = 5        //目标跟踪
+    };
+public:
+    explicit IIPSComm(QObject *parent = nullptr);
+    ~IIPSComm();
+    int SaveWaypointsToJsonFile(PacketId type);
+    Vehicle* getVehicle() const;
+    void setVehicle(Vehicle *value);
+    void AppendVehicle(Vehicle *value);
+    void RemoveVehicle(Vehicle *value);
+    bool getIipsConnected() const;
+    /**
+     * @brief SendAck 发送任务上传的应答
+     */
+    void SendAck();
+    /**
+     * @brief GetWayPoints 从数据包中获取完整的航点列表，包括开始、航点、结束判断
+     * @param type  只分航点（Line）与区域两种
+     * @param true:获取成功 false:接收获取失败，id不匹配
+     */
+    bool GetWayPoints(PacketId type);
+
+public slots:
+    void SlotSendData(const QGCStatusData &data);
+    void SlotReceiveData();
+    void SlotTestTimeout();
+private:
+    const uint8_t heartbeatPacketId = 0;              //IIPS心跳数据包编号
+//    const uint8_t commandPacketId = 128;              //IIPS命令数据包编号
+    const uint8_t commandPacketLength = 51;           //IIPS命令数据包长度
+    const uint8_t statusPacketId = 128;               //QGC状态数据包编号
+    const uint8_t statusPacketLength = 71;            //QGC状态数据包长度
+
+    /* IIPS任务上传的开始、发送与结束消息编号都是4，是通过数据包长度及数据包中且data[10]来区分，
+     * 开始上传数据包长度为11 且data[10]=0，航点上传数据包长度为31且data[10]=1，上传结束数据包长度为11且data[10]=2
+     * qgc请求数据包编号为3，数据包长度为11，data[10]=0xFF
+    */
+    const uint8_t missionUploadPacketId = 4;                //IIPS任务上传数据包编号
+    const uint8_t missionUploadLength = 11;                 //数据长度
+    const uint8_t missionUploadWaypointLength = 31;         //路点数据长度
+
+    const uint8_t missionUploadResponsePacketId = 129;      //qgc发送至IIPS的航线任务请求数据包编号
+    const uint8_t missionUploadResponsePacketLength = 11;   //qgc发送至IIPS的航线任务请求数据包长度
+
+
+    uint16_t sysId = 0;       //载具编号
+    QList<Waypoint> listWaypoints;
+    Vehicle* vehicle = nullptr;
+    QList<Vehicle*> vehicles;
+    QGCStatusData data;
+
+    /******************** 通信相关 ********************/
+    BinaryPacket sendPacket;
+    BinaryPacket receivePacket;
+    BinaryBuffer receiveBuffer;
+    QGCStatusData statusData;
+    IIPSProtocol iipsProtocol;
+    QUdpSocket udpSocket;
+    QString iipsIp = "127.0.0.1";   //综合信息处理系统默认ip地址
+    QHostAddress iipsAddress;
+    uint16_t iipsPort = 8000;       //综合信息处理系统默认端口
+    uint16_t qgcPort = 8001;        //QGroundControl默认端口
+    bool iipsConnected = false;     //综合信息处理系统是否已连接
+    const int iipsTimeOut = 3;      //超过此事件认为综合信息处理系统已断开，图标消失
+    QTimer timerIipsConnect;
+signals:
+    void sigSendPlanFile(const QString fileName);
+    void iipsConnectedChanged();
+};
+
